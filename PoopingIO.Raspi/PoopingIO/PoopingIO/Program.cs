411fb55253580f31c3feb27273811eb124e80422
Ramiro Flores <Serk352@gmail.com>
2019-07-18 18:26:20 -0500

    baths

--- PoopingIO.Raspi/PoopingIO/PoopingIO/Program.cs
+++ PoopingIO.Raspi/PoopingIO/PoopingIO/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
 using Unosquare.RaspberryIO;
 using Unosquare.RaspberryIO.Abstractions;
 using Unosquare.RaspberryIO.Peripherals;
@@ -11,50 +12,87 @@ namespace PoopingIO
 {
     class Program
     {
+        private const string postUrl = "http://21b00c23.ngrok.io/api/bathroom/update";
+        private const int mBathId = 1;
+        private const int wBathId = 2;
+
+        //Pins used to create the doors buttons
+        private static IGpioPin door0Pin = Pi.Gpio[BcmPin.Gpio12];
+        private static IGpioPin door1Pin = Pi.Gpio[BcmPin.Gpio05];
+        private static IGpioPin door2Pin = Pi.Gpio[BcmPin.Gpio06];
+        private static IGpioPin door3Pin = Pi.Gpio[BcmPin.Gpio13];
+
+        private static Dictionary<string, bool> mDoorsState = new Dictionary<string, bool>()
+        {
+            { "door 0", door0Pin.Read() },
+            { "door 1", door1Pin.Read() },
+        };
+
+        private static Dictionary<string, bool> wDoorsState = new Dictionary<string, bool>()
+        {
+            { "door 2", door2Pin.Read() },
+            { "door 3", door3Pin.Read() },
+        };
 
         static void Main(string[] args)
         {
-            var postUrl = "";
 
-            //Pins used to read the doors state
-            var door0Pin = Pi.Gpio[BcmPin.Gpio12];
-            var door1Pin = Pi.Gpio[BcmPin.Gpio05];
-            var door2Pin = Pi.Gpio[BcmPin.Gpio06];
-            var door3Pin = Pi.Gpio[BcmPin.Gpio13];
+            InitialSync();
 
-            var doorsState = new Dictionary<string, bool>()
+            var mbDoorsButtons = new Dictionary<string, Button>()
+              {
+                  { "door 0", new Button(door0Pin) },
+                  { "door 1", new Button(door1Pin) },
+              };
+
+            var wbDoorsButtons = new Dictionary<string, Button>()
             {
-                    { "door 0", door0Pin.Read() },
-                    { "door 1", door1Pin.Read() },
-                    { "door 2", door2Pin.Read() },
-                    { "door 3", door3Pin.Read() },
-            };
-
-            var doorsButtons = new Dictionary<string, Button>()
-            {
-                { "door 0", new Button(door0Pin) },
-                { "door 1", new Button(door1Pin) },
                 { "door 2", new Button(door2Pin) },
                 { "door 3", new Button(door3Pin) },
             };
-            var response = "";
-            doorsButtons.ForEach((key, value) =>
+
+            mbDoorsButtons.ForEach((key, value) =>
             {
                 value.Pressed += (s, e) =>
                 {
-                    doorsState[key] = true;
-                    JsonClient.Post<Dictionary<string, bool>>(postUrl, doorsState); //needs to be waited and validate if the server is working
+                    mDoorsState[key] = true;
+                    var availableDoors = mDoorsState.Where(x => x.Value == true).ToArray().Length;
+                    JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
                 value.Released += (s, e) =>
                 {
-                    doorsState[key] = false;
-                    JsonClient.Post<Dictionary<string, bool>>(postUrl, doorsState);
+                    mDoorsState[key] = false;
+                    var availableDoors = mDoorsState.Where(x => x.Value == true).ToArray().Length;
+                    JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
-
             });
 
+            wbDoorsButtons.ForEach((key, value) =>
+            {
+                value.Pressed += (s, e) =>
+                {
+                    wDoorsState[key] = true;
+                    var availableDoors = wDoorsState.Where(x => x.Value == true).ToArray().Length;
+                    JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
+                };
+                value.Released += (s, e) =>
+                {
+                    wDoorsState[key] = false;
+                    var availableDoors = wDoorsState.Where(x => x.Value == true).ToArray().Length;
+                    JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
+                };
+            });
 
+            Console.ReadKey();
+        }
 
+        static void InitialSync()
+        {
+            var mbavailable = mDoorsState.Where(x => x.Value == true).ToArray().Length;
+            var wbavailable = wDoorsState.Where(x => x.Value == true).ToArray().Length;
+
+            JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{mbavailable}", new { }).GetAwaiter().GetResult();
+            JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{wbavailable}", new { }).GetAwaiter().GetResult();
         }
     }
 }
