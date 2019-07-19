2155f88d99a99e5d30e30c1dce065ca6c065c47b
JCorrea13 <josemanuelcorrea.13@gmail.com>
2019-07-19 11:35:59 -0500

    final

---
+++ PoopingIO.Raspi/PoopingIO/PoopingIO/PoopingMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Abstractions;
+using Unosquare.Swan;
+
+namespace PoopingIO
+{
+    class PoopingMain
+    {
+        public static void PMain()
+        {
+            var pin = Pi.Gpio[BcmPin.Gpio12];
+            pin.PinMode = GpioPinDriveMode.Input;
+            pin.RegisterInterruptCallback(EdgeDetection.FallingEdge, ISRCallback);
+            Console.ReadKey();
+        }
+
+        static void ISRCallback()
+        {
+            "Pin Activated...".WriteLine();
+        }
+
+    }
+}
--- PoopingIO.Raspi/PoopingIO/PoopingIO/Program.cs
+++ PoopingIO.Raspi/PoopingIO/PoopingIO/Program.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Unosquare.RaspberryIO;
 using Unosquare.RaspberryIO.Abstractions;
-using Unosquare.RaspberryIO.Peripherals;
 using Unosquare.Swan;
 using Unosquare.Swan.Networking;
-
+using Unosquare.WiringPi;
 
 namespace PoopingIO
 {
@@ -16,53 +15,58 @@ namespace PoopingIO
         private const int mBathId = 1;
         private const int wBathId = 2;
 
-        //Pins used to create the doors buttons
-        private static IGpioPin door0Pin = Pi.Gpio[BcmPin.Gpio12];
-        private static IGpioPin door1Pin = Pi.Gpio[BcmPin.Gpio05];
-        private static IGpioPin door2Pin = Pi.Gpio[BcmPin.Gpio06];
-        private static IGpioPin door3Pin = Pi.Gpio[BcmPin.Gpio13];
 
-        private static Dictionary<string, bool> mDoorsState = new Dictionary<string, bool>()
-        {
-            { "door 0", door0Pin.Read() },
-            { "door 1", door1Pin.Read() },
-        };
 
-        private static Dictionary<string, bool> wDoorsState = new Dictionary<string, bool>()
-        {
-            { "door 2", door2Pin.Read() },
-            { "door 3", door3Pin.Read() },
-        };
 
         static void Main(string[] args)
         {
-           // Pi.Init<BootstrapWiringPi>();
+            Pi.Init<BootstrapWiringPi>();
+            
+            //Pins used to create the doors buttons
+        var door0Pin = Pi.Gpio[BcmPin.Gpio12];
+        var door1Pin = Pi.Gpio[BcmPin.Gpio05];
+        var door2Pin = Pi.Gpio[BcmPin.Gpio06];
+        var door3Pin = Pi.Gpio[BcmPin.Gpio13];
 
-            InitialSync();
+            var mDoorsState = new Dictionary<string, bool>()
+        {
+            { "door 0", !door0Pin.Read() },
+            { "door 1", !door1Pin.Read()},
+        };
+
+        var wDoorsState = new Dictionary<string, bool>()
+        {
+            { "door 2", !door2Pin.Read()},
+            { "door 3", !door3Pin.Read()},
+        };
+
+            "antes del sync".Info();
+            InitialSync(mDoorsState, wDoorsState);
+            "despues del sync".Info();
 
             var mbDoorsButtons = new Dictionary<string, Button>()
               {
-                  { "door 0", new Button(door0Pin) },
-                  { "door 1", new Button(door1Pin) },
+                  { "door 0", new Button(door0Pin, GpioPinResistorPullMode.PullDown) },
+                  { "door 1", new Button(door1Pin, GpioPinResistorPullMode.PullDown) },
               };
 
             var wbDoorsButtons = new Dictionary<string, Button>()
             {
-                { "door 2", new Button(door2Pin) },
-                { "door 3", new Button(door3Pin) },
+                { "door 2", new Button(door2Pin, GpioPinResistorPullMode.PullDown) },
+                { "door 3", new Button(door3Pin, GpioPinResistorPullMode.PullDown) },
             };
-
+            "buttons".Info();
             mbDoorsButtons.ForEach((key, value) =>
             {
                 value.Pressed += (s, e) =>
                 {
-                    mDoorsState[key] = true;
+                    mDoorsState[key] = false;
                     var availableDoors = mDoorsState.Where(x => x.Value == true).ToArray().Length;
                     JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
                 value.Released += (s, e) =>
                 {
-                    mDoorsState[key] = false;
+                    mDoorsState[key] = true;
                     var availableDoors = mDoorsState.Where(x => x.Value == true).ToArray().Length;
                     JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
@@ -72,13 +76,13 @@ namespace PoopingIO
             {
                 value.Pressed += (s, e) =>
                 {
-                    wDoorsState[key] = true;
+                    wDoorsState[key] = false;
                     var availableDoors = wDoorsState.Where(x => x.Value == true).ToArray().Length;
                     JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
                 value.Released += (s, e) =>
                 {
-                    wDoorsState[key] = false;
+                    wDoorsState[key] = true;
                     var availableDoors = wDoorsState.Where(x => x.Value == true).ToArray().Length;
                     JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{availableDoors}", new { }).GetAwaiter().GetResult();
                 };
@@ -87,13 +91,18 @@ namespace PoopingIO
             Console.ReadKey();
         }
     
-        static void InitialSync()
+        static void InitialSync(Dictionary<string, bool> mDoorsState, Dictionary<string, bool>  wDoorsState )
         {
             var mbavailable = mDoorsState.Where(x => x.Value == true).ToArray().Length;
             var wbavailable = wDoorsState.Where(x => x.Value == true).ToArray().Length;
 
+            
             JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{mBathId}/{mbavailable}", new { }).GetAwaiter().GetResult();
             JsonClient.Post<Dictionary<string, bool>>(postUrl + $"/{wBathId}/{wbavailable}", new { }).GetAwaiter().GetResult();
         }
+
+            //PoopingMain.PMain();
+            //Console.ReadKey();
+        
     }
 }
